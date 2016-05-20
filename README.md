3cc38f6d1bf75d73580436bace14e6440a697397
Jonathan Cawalla <Jonathan Cawalla>
2016-05-20 18:11:04 +0200

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ConnectFourGame
+
+JavaDoc GameGrid here: http://www.aplu.ch/classdoc/jgamegrid/index.html
---
+++ src/gameWithAi/BG.java
@@ -0,0 +1,19 @@
+package gameWithAi;
+
+
+
+import ch.aplu.jgamegrid.Actor;
+
+public class BG extends Actor
+{
+  public BG()
+  {
+    super(false, "sprites/4inARowBG.png");
+  }
+
+  public void reset()
+  {
+    this.setLocationOffset(new java.awt.Point(0, 4 * 70));
+    this.setOnTop();
+  }
+}
