3eaf9b3f2d1b02baf91c33bc187b62ab3dea39da
ahmad-emanuel <ahmad.emanuel@gmail.com>
2016-05-27 20:45:23 +0200

    Binärcode final Version

--- src/ConvertArrays/BAPArrayas.java
+++ src/ConvertArrays/BAPArrayas.java
@@ -5,62 +5,51 @@ import java.util.*;
 
 public class BAPArrayas {
     public static void main(String[] args) {
-        int [][] x = {{0,0,0},{0,0,1},{1,2,2}};
-        int [][] x1= new int [5][4];
+        int [][] x = {{0,0,1,1},{1,1,2,1},{2,1,2,1},{1,2,1,2},{1,2,1,2}};
         
+        System.out.println("x:");
         ShowArray(x);
-        System.out.println(""); 
-        ConvertArray (x);
         
+        int[] y;
+        y = ConvertArray (x);
         
-   
-       
-     
+        System.out.println(Arrays.toString(y)); 
+        
         }
     
-    public static void ConvertArray (int [][] array){
+public static int [] ConvertArray (int [][] array){
     
-  int width = array.length;
-  int height = array[0].length;
+  int height = array.length;
+  int width = array[0].length;
 
-  int[][] array_1 = new int[height][width];
-  int[][] array_2 = new int[height][width];
+  int[][] MatrixOfOnes = new int[height][width];
+  int[][] MatrixOfTwoes = new int[height][width];
 
-  for (int i = 0; i < width; i++) {
-    for (int j = 0; j < height; j++) {    
-          if (array[i][j] == 1) {array_1[i][j] = 1;}
-     else if (array[i][j] == 2) { array_2 [i][j] = 1;}
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; j < width; j++) {    
+          if (array[i][j] == 1) {MatrixOfOnes[i][j] = 1;}
+     else if (array[i][j] == 2) {MatrixOfTwoes[i][j] = 1;}
     }
   }
-  array_1 = Transpose(array_1);
-  array_2 = Transpose(array_2);
+  MatrixOfOnes = Transpose(MatrixOfOnes);
+  MatrixOfTwoes = Transpose(MatrixOfTwoes);
   
-    int l = array_1[0].length;
-    int sum1 = 0;
-    int sum2 = 0;
-    int [] A_1 = new int [array_1.length];
-    int [] A_2 = new int [array_2.length];
-  
-        for (int k = 0; k < array_1.length ; k++) {
-            while ((l-1) >= 0 ) {
-            sum1 += ( ((int) Math.pow(2, l) ) *array_1[k][l]);
-            sum2 += ( ((int) Math.pow(2, l) ) *array_2[k][l]);
-                l--;        
-            }
-        A_1 [k] = sum1;
-        A_2 [k] = sum2;
-        sum1 = 0;
-        sum2 = 0;
-        }
-  
-  ShowArray(array_1);
-  ShowArray(array_2);
-  
-  System.out.println(Arrays.toString(A_1));
-  System.out.println(Arrays.toString(A_2));  
-  }
+    int [] BinaryCodeOnes = new int [MatrixOfOnes.length];
+    int [] BinaryCodeTwoes = new int [MatrixOfTwoes.length];
     
-    public static int[][] Transpose (int[][] array) {
+    for (int k = 0; k < MatrixOfOnes.length ; k++) {
+        for (int l = MatrixOfOnes[0].length-1; l >=0 ; l--) {
+            BinaryCodeOnes[k] += ( ((int) Math.pow(2, MatrixOfOnes[0].length - l - 1) ) * MatrixOfOnes[k][l]);
+            BinaryCodeTwoes[k] += ( ((int) Math.pow(2, MatrixOfOnes[0].length - l -1) ) * MatrixOfTwoes[k][l]);
+        }
+    }
+    int [] BinaryCode = new int [BinaryCodeOnes.length + BinaryCodeTwoes.length];
+    System.arraycopy(BinaryCodeOnes, 0, BinaryCode, 0, BinaryCodeOnes.length);
+    System.arraycopy(BinaryCodeTwoes, 0, BinaryCode, BinaryCodeOnes.length, BinaryCodeTwoes.length);
+    return BinaryCode;
+    }
+    
+ public static int[][] Transpose (int[][] array) {
   if (array == null || array.length == 0)//empty or unset array, nothing do to here
     return array;
 
