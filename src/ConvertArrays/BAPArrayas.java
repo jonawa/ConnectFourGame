c2e778bb09884bbf771b60b8a2bb81caa05fb1ea
ahmad-emanuel <ahmad.emanuel@gmail.com>
2016-05-31 12:27:33 +0200

    Binärcode 2. final Version

--- src/ConvertArrays/BAPArrayas.java
+++ src/ConvertArrays/BAPArrayas.java
@@ -1,5 +1,3 @@
-package ConvertArrays;
-
 
 import java.util.*;
 
@@ -10,19 +8,15 @@ public class BAPArrayas {
         System.out.println("x:");
         ShowArray(x);
         
-<<<<<<< HEAD
         int[] y;
         y = ConvertArray (x);
         
         System.out.println(Arrays.toString(y)); 
         
-=======
->>>>>>> 8fcd55fc3b8f83f3da7638cbf75ca8480cf8e465
         }
     
 public static int [] ConvertArray (int [][] array){
     
-<<<<<<< HEAD
   int height = array.length;
   int width = array[0].length;
 
@@ -68,66 +62,6 @@ public static int [] ConvertArray (int [][] array){
     }
   }
   return array_new;
-=======
-    	int width = array.length;
-    	int height = array[0].length;
-
-    	int[][] array_1 = new int[height][width];
-    	int[][] array_2 = new int[height][width];
-
-    	for (int i = 0; i < width; i++) {
-    		for (int j = 0; j < height; j++) {    
-    				if (array[i][j] == 1) 
-    					{array_1[i][j] = 1;}
-    				else if (array[i][j] == 2) 
-    					{ array_2 [i][j] = 1;}
-    			}
-    	}
-    	array_1 = Transpose(array_1);
-    	array_2 = Transpose(array_2);
-  
-    	int l = array_1[0].length;
-    	int sum1 = 0;
-    	int sum2 = 0;
-    	int [] A_1 = new int [array_1.length];
-    	int [] A_2 = new int [array_2.length];
-  
-        for (int k = 0; k < array_1.length ; k++) {
-            while ((l-1) >= 0 ) {
-            	sum1 += ( ((int) Math.pow(2, l) ) *array_1[k][l]);
-            	sum2 += ( ((int) Math.pow(2, l) ) *array_2[k][l]);
-                l--;        
-            }
-            A_1 [k] = sum1;
-            A_2 [k] = sum2;
-            sum1 = 0;
-            sum2 = 0;
-        }
-  
-        ShowArray(array_1);
-        ShowArray(array_2);
-  
-        System.out.println(Arrays.toString(A_1));
-        System.out.println(Arrays.toString(A_2));  
-  }
-    
-    public static int[][] Transpose (int[][] array) {
-    	
-	  if (array == null || array.length == 0)//empty or unset array, nothing do to here
-		  return array;
-	
-	  int width = array.length;
-	  int height = array[0].length;
-	
-	  int[][] array_new = new int[height][width];
-	
-	  for (int i = 0; i < width; i++) {
-		  for (int j = 0; j < height; j++) {
-			  array_new[j][i] = array[i][j];
-	    }
-	  }
-	  return array_new;
->>>>>>> 8fcd55fc3b8f83f3da7638cbf75ca8480cf8e465
 }    
     
     public static void ShowArray (int [][] A){
