86ea83a1b9d525587b4b6759de0dacb5ee8b4600
ahmad-emanuel <ahmad.emanuel@gmail.com>
2016-06-08 00:21:21 +0200

    Binärkode 3. final version. Nethoden zu beiden Umwandlungen vorhanden. Sehr vereinfacht und optimiert

--- src/ConvertArrays/BAPArrayas.java
+++ src/ConvertArrays/BAPArrayas.java
@@ -1,77 +1,79 @@
 package ConvertArrays;
 
-
-import java.util.*;
-
 public class BAPArrayas {
-    public static void main(String[] args) {
-        int [][] x = {{0,0,1,1},{1,1,2,1},{2,1,2,1},{1,2,1,2},{1,2,1,2}};
+	public static void main(String[] args) {
         
-        System.out.println("x:");
-        ShowArray(x);
+        int [][] y = {{0,0,1,1},{1,1,-1,1},{-1,1,-1,1},{1,-1,1,-1},{1,-1,1,-1}};
+        int [][] x;
+        int [] binar;
         
-        int[] y;
-        y = ConvertArray (x);
         
-        System.out.println(Arrays.toString(y)); 
+        System.out.print("y = \n");
+        ShowArray (y);
         
-        }
-    
-public static int [] ConvertArray (int [][] array){
-    
-  int height = array.length;
-  int width = array[0].length;
-
-  int[][] MatrixOfOnes = new int[height][width];
-  int[][] MatrixOfTwoes = new int[height][width];
-
-  for (int i = 0; i < height; i++) {
-    for (int j = 0; j < width; j++) {    
-          if (array[i][j] == 1) {MatrixOfOnes[i][j] = 1;}
-     else if (array[i][j] == 2) {MatrixOfTwoes[i][j] = 1;}
-    }
-  }
-  MatrixOfOnes = Transpose(MatrixOfOnes);
-  MatrixOfTwoes = Transpose(MatrixOfTwoes);
-  
-    int [] BinaryCodeOnes = new int [MatrixOfOnes.length];
-    int [] BinaryCodeTwoes = new int [MatrixOfTwoes.length];
-    
-    for (int k = 0; k < MatrixOfOnes.length ; k++) {
-        for (int l = MatrixOfOnes[0].length-1; l >=0 ; l--) {
-            BinaryCodeOnes[k] += ( ((int) Math.pow(2, MatrixOfOnes[0].length - l - 1) ) * MatrixOfOnes[k][l]);
-            BinaryCodeTwoes[k] += ( ((int) Math.pow(2, MatrixOfOnes[0].length - l -1) ) * MatrixOfTwoes[k][l]);
-        }
-    }
-    int [] BinaryCode = new int [BinaryCodeOnes.length + BinaryCodeTwoes.length];
-    System.arraycopy(BinaryCodeOnes, 0, BinaryCode, 0, BinaryCodeOnes.length);
-    System.arraycopy(BinaryCodeTwoes, 0, BinaryCode, BinaryCodeOnes.length, BinaryCodeTwoes.length);
-    return BinaryCode;
+        binar = ArraytoBinary (y);
+        System.out.print("\nBinary = \n");
+        ShowArray(binar);
+        
+        x = BinarytoArray (binar);
+        System.out.print("\nx = \n");
+        ShowArray (x);
+        
+        
+        
     }
     
- public static int[][] Transpose (int[][] array) {
-  if (array == null || array.length == 0)//empty or unset array, nothing do to here
-    return array;
-
-  int width = array.length;
-  int height = array[0].length;
-
-  int[][] array_new = new int[height][width];
-
-  for (int i = 0; i < width; i++) {
-    for (int j = 0; j < height; j++) {
-      array_new[j][i] = array[i][j];
-    }
-  }
-  return array_new;
-}    
-    
-    public static void ShowArray (int [][] A){
-        for (int i = 0; i < A.length; i++){
-            for (int j = 0; j < A[i].length; j++) {
-                System.out.print(A[i][j] + " ");
+    public static int [] ArraytoBinary (int [][] y){
+        
+        int [] binary = new int [2 * y[0].length];
+        
+        for (int i = 0; i < y[0].length; i++)
+            for (int j = 0; j < y.length; j++){
+                if (y [j][i] == 1) {
+                    binary [i] += (Math.pow(2, y.length-j-1));
+                }   else { if (y [j][i] == -1)
+                    binary [i + y[0].length] += (Math.pow(2, y.length-j-1));
+                    }
             }
-            System.out.println("");
+    return binary; 
+    }
+    
+    public static int [][] BinarytoArray (int [] bin){
+        //Copy of input
+        int [] binary = new int [bin.length];
+        for (int i = 0; i < bin.length; i++) {
+            binary [i] = bin [i];
         }
+        // output Array
+        int [][] x = new int [5][binary.length/2];
+        
+        for (int i = 0; i < (binary.length/2); i++) {
+            for (int j = 0; j < x.length; j++) {
+                if (binary[i] % 2 == 1)
+                    x[x.length-1-j][i] = 1;
+                binary[i] /= 2;
+            }
+            for (int j = 0; j < x.length; j++) {
+                if (binary[i + (binary.length/2)] % 2 == 1)
+                    x[x.length-1-j][i] = -1;
+                binary[i + (binary.length/2)] /= 2;
+            }
+        }
+    return x;
     }
+    
+    public static void ShowArray ( int [] x){
+        
+        for (int i = 0; i < x.length; i++)
+            System.out.print("[" + x[i] + "]");
+        System.out.println("");
     }
+    public static void ShowArray ( int [][] x){
+        
+        for (int i = 0; i < x.length; i++){
+            for (int j = 0; j < x[0].length; j++)
+                System.out.print("[" + x[i][j] + "]");
+            System.out.println("");
+        }        
+    }
+}
